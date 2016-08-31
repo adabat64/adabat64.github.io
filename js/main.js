001bd028158ffc4267b592602d1f147535470af1
adabat64 <adrian.dabat@gmail.com>
2016-08-31 16:40:58 -0700

    angular test

--- js/main.js
+++ js/main.js
@@ -1,38 +1,55 @@
-/*Main AngularJS Web Application*/
+/**
+ * AngularJS Tutorial 1
+ * @author Nick Kaye <nick.c.kaye@gmail.com>
+ */
 
-var app = angular.module('mywebapp', ['ngRoute']);
+/**
+ * Main AngularJS Web Application
+ */
+var app = angular.module('tutorialWebApp', [
+  'ngRoute'
+]);
 
-/*Configure the Routes*/
-app.config(function($routeProvider) {
-	$routeProvider
-		.when("/", {
-			templateUrl: "partials/home.html",
-			controller: "homeController"
-		})
-		.when('/about', {
-			templateUrl: 'partials/about.html',
-			controller: 'aboutController'
-		})
-		.when('/project', {
-			templateUrl: 'partials/projects.html',
-			controller: "projectsController"
-		})
-		.when('/contact', {
-			templateUrl: 'partials/contact.html',
-			controller: 'contactController'
-		});
-});
-app.controller('homeController', function($scope) {
-	$scope.name = "home";
-});
-app.controller('aboutController', function($scope) {
-	$scope.name = "about";
-});
-app.controller('projectsController', function($scope) {
-	$scope.name = "projects";
-});
-app.controller('contactController', function($scope) {
-	$scope.name = "contact";
+/**
+ * Configure the Routes
+ */
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    // Home
+    .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
+    // Pages
+    .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
+    .when("/faq", {templateUrl: "partials/faq.html", controller: "PageCtrl"})
+    .when("/pricing", {templateUrl: "partials/pricing.html", controller: "PageCtrl"})
+    .when("/services", {templateUrl: "partials/services.html", controller: "PageCtrl"})
+    .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
+    // Blog
+    .when("/blog", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
+    .when("/blog/post", {templateUrl: "partials/blog_item.html", controller: "BlogCtrl"})
+    // else 404
+    .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
+}]);
+
+/**
+ * Controls the Blog
+ */
+app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
+  console.log("Blog Controller reporting for duty.");
 });
 
-var crud = angular.module('crud', ['ngResource']);
+/**
+ * Controls all other Pages
+ */
+app.controller('PageCtrl', function (/* $scope, $location, $http */) {
+  console.log("Page Controller reporting for duty.");
+
+  // Activates the Carousel
+  $('.carousel').carousel({
+    interval: 5000
+  });
+
+  // Activates Tooltips for Social Links
+  $('.tooltip-social').tooltip({
+    selector: "a[data-toggle=tooltip]"
+  })
+});
