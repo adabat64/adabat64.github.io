31f07005013a62e71524e3e25d676b4211c2ec40
adabat64 <adrian.dabat@gmail.com>
2016-09-01 10:50:38 -0700

    test angular anchors

--- js/main.js
+++ js/main.js
@@ -18,9 +18,9 @@ app.config(['$routeProvider', function ($routeProvider) {
     // Home
     .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
     // Pages
-    .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
-    .when("/portfolio", {templateUrl: "partials/portfolio.html", controller: "PageCtrl"})
-    .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
+    .when("/about", {templateUrl: "#about", controller: "PageCtrl"})
+    .when("/portfolio", {templateUrl: "#portfolio", controller: "PageCtrl"})
+    .when("/contact", {templateUrl: "#contact", controller: "PageCtrl"})
     // else 404
     .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
 }]);
