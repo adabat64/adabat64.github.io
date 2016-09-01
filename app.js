68f8881c2664a7dfe94a997859156e41f73c00d8
adabat64 <adrian.dabat@gmail.com>
2016-09-01 12:18:40 -0700

    SPA testing

---
+++ app.js
@@ -0,0 +1,30 @@
+var app = angular.module('angularjs-starter', []);
+
+app.config(function($routeProvider) {
+  $routeProvider.when('/test', {
+    controller: 'TestCtrl',
+    templateUrl: 'home.html'
+  })
+  .when('/weee', {
+    controller: 'WeeeCtrl',
+    templateUrl: 'home.html'
+  });
+});
+
+app.run(function($rootScope, $location, $anchorScroll, $routeParams) {
+  $rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
+    $location.hash($routeParams.scrollTo);
+    $anchorScroll();
+  });
+})
+
+app.controller('MainCtrl', function($scope, $location, $anchorScroll, $routeParams) {
+});
+
+
+app.controller('TestCtrl', function($scope) {
+});
+
+
+app.controller('WeeeCtrl', function($scope) {
+});
